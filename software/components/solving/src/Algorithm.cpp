d83c064eabdfccc6bfd6f7ad91cacc3b7d84f503
dimembermatt <matthewjkyu@gmail.com>
2019-06-02 09:28:28 -0500

    added updated algorithm

--- software/components/solving/src/Algorithm.cpp
+++ software/components/solving/src/Algorithm.cpp
@@ -21,21 +21,28 @@ void Algorithm::populateMap() {       // creates hamDist maze
     }
 
     for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 16; j++)
-            tremauxMap[i][j] = 0;
+        for(int j = 0; j < 16; j++){
+            for(int k = 0; k < 5; k++)
+                tremauxMap[i][j][k] = 0;
+        }
     }
-    tremauxMap[0][15] = -1;
-    tremauxMap[7][7] = -1;
-    tremauxMap[7][8] = -1;
-    tremauxMap[8][7] = -1;
-    tremauxMap[8][8] = -1;
+    tremauxMap[0][15][0] = -1;
+    tremauxMap[7][7][0] = -1;
+    tremauxMap[7][8][0] = -1;
+    tremauxMap[8][7][0] = -1;
+    tremauxMap[8][8][0] = -1;
+
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++)
+            solutionMap[i][j] = 0;
+    }
 }
 
 // emulate moving through corridor in algorithm history
 void Algorithm::mvCorr() {            // if only one direction is open, move through corridor
     if(openF){  // dead end, has rotated, move forward.
         out[0] = 0;
-        out[1] = 0;
+        out[1] = 1;
     }else if(openL || openR){   // corner, left or right is open.
         if(openL){  // rotate left
             out[0] = 2;
@@ -55,14 +62,10 @@ void Algorithm::mvDEnd() {            // if no way is open, reverse
     out[0] = 2;
     out[1] = 180;
 
-    tremauxMap[currX][currY] = DEAD_END;
-    // set dEnd for blocking out after moving away
-    // dEnd = true;
+    tremauxMap[currX][currY][0] = DEAD_END;
 }
 
 void Algorithm::mvJunct() {           // method for dealing with junctions, where >1 paths are open
-    // int maxVal = DEAD_END;                  // max hamming distance
-
     int rotation = 0;
     bool hasRotated = false;
     // first check if there is a priority among tremaux
@@ -71,9 +74,9 @@ void Algorithm::mvJunct() {           // method for dealing with junctions, wher
     int front, left, right;
     switch(currDir){
         case 0: // east
-            front = tremauxMap[currX + 1][currY];
-            left = tremauxMap[currX][currY - 1];
-            right = tremauxMap[currX][currY + 1];
+            front = tremauxMap[currX + 1][currY][0];
+            left = tremauxMap[currX][currY - 1][0];
+            right = tremauxMap[currX][currY + 1][0];
 
             if(openF)                       // front
                 lowestVal = front;
@@ -99,9 +102,9 @@ void Algorithm::mvJunct() {           // method for dealing with junctions, wher
             }
             break;
         case 1: // north
-            front = tremauxMap[currX][currY - 1];
-            left = tremauxMap[currX - 1][currY];
-            right = tremauxMap[currX + 1][currY];
+            front = tremauxMap[currX][currY - 1][0];
+            left = tremauxMap[currX - 1][currY][0];
+            right = tremauxMap[currX + 1][currY][0];
 
             if(openF)                       // front
                 lowestVal = front;
@@ -127,9 +130,9 @@ void Algorithm::mvJunct() {           // method for dealing with junctions, wher
             }
             break;
         case 2: // west
-            front = tremauxMap[currX - 1][currY];
-            left = tremauxMap[currX][currY + 1];
-            right = tremauxMap[currX][currY - 1];
+            front = tremauxMap[currX - 1][currY][0];
+            left = tremauxMap[currX][currY + 1][0];
+            right = tremauxMap[currX][currY - 1][0];
 
             if(openF)                       // front
                 lowestVal = front;
@@ -155,9 +158,9 @@ void Algorithm::mvJunct() {           // method for dealing with junctions, wher
             }
             break;
         case 3: // south
-            front = tremauxMap[currX][currY + 1];
-            left = tremauxMap[currX + 1][currY];
-            right = tremauxMap[currX - 1][currY];
+            front = tremauxMap[currX][currY + 1][0];
+            left = tremauxMap[currX + 1][currY][0];
+            right = tremauxMap[currX - 1][currY][0];
 
             if(openF)                       // front
                 lowestVal = front;
@@ -186,103 +189,12 @@ void Algorithm::mvJunct() {           // method for dealing with junctions, wher
             cout << "Algorithm::myJunct(): invalid currDir" << endl;
     }
 
-    // switch(currDir){
-    //     case 0:
-    //         if(openF){
-    //             if(hamDist[currX + 1][currY] < maxVal) {    //when hamming distance for east tile is less than the curr max hamdist
-    //                 hasRotated = false;                     //no need to rotate
-    //                 maxVal = hamDist[currX + 1][currY];     //sets new maxval to compare and see if anything is lower
-    //             }
-    //         }
-    //         if(openL){
-    //             if(hamDist[currX][currY - 1] < maxVal) {
-    //                 rotation = 90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX][currY - 1];     //set max val
-    //             }
-    //         }
-    //         if(openR){
-    //             if(hamDist[currX][currY + 1] < maxVal) {    //and current max hamdist is less than current
-    //                 rotation = -90;                          //set rotation to left
-    //                 hasRotated = true;                      //set flag for has rotated
-    //                 maxVal = hamDist[currX][currY + 1];
-    //             }
-    //         }
-    //         break;
-    //     case 1:
-    //         if(openF){
-    //             if(hamDist[currX][currY - 1] < maxVal) {
-    //                 hasRotated = false;
-    //                 maxVal = hamDist[currX][currY - 1];
-    //             }
-    //         }
-    //         if(openL){
-    //             if(hamDist[currX - 1][currY] < maxVal) {
-    //                 rotation = 90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX - 1][currY];
-    //             }
-    //         }
-    //         if(openR){
-    //             if(hamDist[currX + 1][currY] < maxVal) {
-    //                 rotation = -90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX + 1][currY];
-    //             }
-    //         }
-    //         break;
-    //     case 2:
-    //         if(openF){
-    //             if(hamDist[currX - 1][currY] < maxVal) {
-    //                 hasRotated = false;
-    //                 maxVal = hamDist[currX - 1][currY];
-    //             }
-    //         }
-    //         if(openL){
-    //             if(hamDist[currX][currY + 1] < maxVal) {
-    //                 rotation = 90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX][currY + 1];
-    //             }
-    //         }
-    //         if(openR){
-    //             if(hamDist[currX][currY - 1] < maxVal) {
-    //                 rotation = -90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX][currY - 1];
-    //             }
-    //         }
-    //         break;
-    //     case 3:
-    //         if(openF){
-    //             if(hamDist[currX][currY + 1] < maxVal) {
-    //                 hasRotated = false;
-    //                 maxVal = hamDist[currX][currY + 1];
-    //             }
-    //         }
-    //         if(openL){
-    //             if(hamDist[currX + 1][currY] < maxVal) {
-    //                 rotation = 90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX + 1][currY];
-    //             }
-    //         }
-    //         if(openR){
-    //             if(hamDist[currX - 1][currY] < maxVal) {
-    //                 rotation = -90;
-    //                 hasRotated = true;
-    //                 maxVal = hamDist[currX - 1][currY];
-    //             }
-    //         }
-    //         break;
-    // }
     if(hasRotated){
         out[0] = 2;
         out[1] = rotation;
     }else{
         out[0] = 0;
-        out[1] = 0;
-        tremauxMap[currX][currY]++;
+        out[1] = 1;
     }
 }
 
@@ -324,9 +236,9 @@ int Algorithm::numPath() {
             break;
     }
 
-    openL = openL && tremauxMap[left[0]][left[1]] < DEAD_END;
-    openF = openF && tremauxMap[front[0]][front[1]] < DEAD_END;
-    openR = openR && tremauxMap[right[0]][right[1]] < DEAD_END;
+    openL = openL && tremauxMap[left[0]][left[1]][0] < DEAD_END;
+    openF = openF && tremauxMap[front[0]][front[1]][0] < DEAD_END;
+    openR = openR && tremauxMap[right[0]][right[1]][0] < DEAD_END;
 
     if(openL){i++;}
     if(openF){i++;}
@@ -336,30 +248,34 @@ int Algorithm::numPath() {
 
 // update the algorithm hamming map based on displacement.
 void Algorithm::updateMap(){
-    if(currX != prevX){ // only X has changed
-        if(currX-prevX > 1){    // moved east
-            for(int i = prevX + 1; i < currX; i++){
-                if(tremauxMap[i][currY] != -1)
-                    tremauxMap[i][currY]++;
-            }
-        }else{  // moved west
-            for(int i = currX + 1; i < prevX; i++){
-                if(tremauxMap[i][currY] != -1)
-                    tremauxMap[i][currY]++;
-            }
-        }
-    }else{  // only y has changed
-        if(currY-prevY > 1){    // moved south
-            for(int i = prevY + 1; i < currY; i++){
-                if(tremauxMap[currX][i] != -1)
-                    tremauxMap[currX][i]++;
-            }
-        }else{  // moved north
-            for(int i = currY + 1; i < prevY; i++){
-                if(tremauxMap[currX][i] != -1)
-                    tremauxMap[currX][i]++;
-            }
-        }
+    switch(currDir){
+        case 0: // east
+            tremauxMap[currX][currY][1] = openF;
+            tremauxMap[currX][currY][2] = openL;
+            tremauxMap[currX][currY][3] = 1;
+            tremauxMap[currX][currY][4] = openR;
+            break;
+        case 1: // north
+            tremauxMap[currX][currY][1] = openR;
+            tremauxMap[currX][currY][2] = openF;
+            tremauxMap[currX][currY][3] = openL;
+            tremauxMap[currX][currY][4] = 1;
+            break;
+        case 2: // west
+            tremauxMap[currX][currY][1] = 1;
+            tremauxMap[currX][currY][2] = openR;
+            tremauxMap[currX][currY][3] = openF;
+            tremauxMap[currX][currY][4] = openL;
+            break;
+        case 3: // south
+            tremauxMap[currX][currY][1] = openL;
+            tremauxMap[currX][currY][2] = 1;
+            tremauxMap[currX][currY][3] = openR;
+            tremauxMap[currX][currY][4] = openF;
+            break;
+        default:
+            cout << "Algorithm::updateMap(): Invalid restricted heading - " << currDir << endl;
+            break;
     }
 }
 
@@ -370,28 +286,34 @@ Algorithm::Algorithm() {
 
 int* Algorithm::decide() {
     int numPaths = numPath();
+    cout << "Internal Algo Pos (X|Y|dir): " << currX << "|" << currY << "|" << currDir << endl;
+    cout << "Num Paths: " << numPaths << endl;
+
     switch(numPaths){
         case 0: // dead end (turn around!)
             mvDEnd();
             break;
         case 1: // corridor (move forward after hitting a dead end)
             mvCorr();
+            if(tremauxMap[currX][currY][0] != -1)
+                tremauxMap[currX][currY][0]++;
             break;
         case 2:
         case 3: // junction (decide where to go!)
             mvJunct();
+            if(tremauxMap[currX][currY][0] != -1)
+                tremauxMap[currX][currY][0]++;
             break;
         default:
             cout << "Algorithm::decide(): Invalid numPath (" << numPath() << ")." << endl;
             break;
     }
 
-    if(hamDist[currX][currY] == 0){ //if reached center
-        out[0] = 3;                 //set finish flag
+    if(hamDist[currX][currY] == 0){ //if reached center, set finish flag
+        out[0] = 3;
     }
 
-    // cout << "Internal Algo Pos (X|Y|dir): " << currX << "|" << currY << "|" << currDir << endl;
-    // cout << "Decision: " << out[0] << "|" << out[1] << endl;
+    cout << "Decision: " << out[0] << "|" << out[1] << endl;
     return out;
 }
 
@@ -450,16 +372,111 @@ void Algorithm::printMaze(){
                         cout << " \u2193\t";
                         break;
                 }}
-            else if(tremauxMap[j][i] >= DEAD_END)
+            else if(tremauxMap[j][i][0] >= DEAD_END)
                 cout << "\u25A0" << "\t";
-            else cout << tremauxMap[j][i] << "\t";
+            else cout << tremauxMap[j][i][0] << "\t";
         }
         cout << endl;
     }
     cout << "--- end tremaux map ---" << endl;
 }
 
-std::vector<Decision> Algorithm::getShortestPath(){
+// BFS implementation modified from https://www.geeksforgeeks.org/shortest-path-in-a-binary-maze/.
+std::stack<Point> Algorithm::getShortestPath(){
+    std::queue<Point> queue;
 
+    // start with cell at 0|15 (X).
+    struct Point p = {0, 15};
+    queue.push(p);
+
+    Point endPoint;
+
+    // iterative: while the queue is not empty
+    int round = 1;
+    bool nEnd = true;
+    int size = 1;
+
+    while(size > 0 && nEnd){
+        // for each point in the queue, assign it a value if it has not already received one
+        size = queue.size();
+        for(int i = 0; i < size; i++){
+            struct Point pt = queue.front();
+            queue.pop();
+
+            // if endpoint is found, exit.
+            if(tremauxMap[pt.x][pt.y][0] == -1 && pt.x != 0 && pt.y != 15){
+                solutionMap[pt.x][pt.y] = round;
+                endPoint = pt;
+                nEnd = false;
+                break;
+            }
+
+            if(pt.x >= 0 && pt.x <= 15 && pt.y >= 0 && pt.y <= 15
+                && tremauxMap[pt.x][pt.y][0] != 0 && solutionMap[pt.x][pt.y] == 0){
+                    // set value of flood fill iteration
+                    solutionMap[pt.x][pt.y] = round;
+
+                    // find all nearby possible path points and add to queue.
+                    for(int nearby = 0; nearby < 4; nearby++){
+                        switch(nearby){
+                            case 0: // E
+                                // and is an open path, and has been explored, and is not part of another solution
+                                if(tremauxMap[pt.x][pt.y][1] == 1){
+                                    struct Point p = {pt.x + 1, pt.y};
+                                    queue.push(p);
+                                }
+                                break;
+                            case 1: // N
+                                // and is an open path, and has been explored, and is not part of another solution
+                                if(tremauxMap[pt.x][pt.y][2] == 1){
+                                    struct Point p = {pt.x, pt.y - 1};
+                                    queue.push(p);
+                                }
+                                break;
+                            case 2: // W
+                                // and is an open path, and has been explored, and is not part of another solution
+                                if(tremauxMap[pt.x][pt.y][3] == 1){
+                                    struct Point p = {pt.x - 1, pt.y};
+                                    queue.push(p);
+                                }
+                                break;
+                            case 3: // S
+                                // and is an open path, and has been explored, and is not part of another solution
+                                if(tremauxMap[pt.x][pt.y][4] == 1){
+                                    struct Point p = {pt.x, pt.y + 1};
+                                    queue.push(p);
+                                }
+                                break;
+                        }
+                    }
+            }
+        }
+        round++;
+    }
+
+    do{
+        // push the next last point onto the stack
+        solution.push(endPoint);
+        // find the next adjacent neighbor (E/N/W/S)
+        int val = solutionMap[endPoint.x][endPoint.y];
+        if(val == solutionMap[endPoint.x + 1][endPoint.y] + 1){
+            struct Point p = {endPoint.x + 1, endPoint.y};
+            endPoint = p;
+        }else if(val == solutionMap[endPoint.x][endPoint.y - 1] + 1){
+            struct Point p = {endPoint.x, endPoint.y - 1};
+            endPoint = p;
+        }else if(val == solutionMap[endPoint.x - 1][endPoint.y] + 1){
+            struct Point p = {endPoint.x - 1, endPoint.y};
+            endPoint = p;
+        }else if(val == solutionMap[endPoint.x][endPoint.y + 1] + 1){
+            struct Point p = {endPoint.x, endPoint.y + 1};
+            endPoint = p;
+        }
+    }while(!(endPoint.x == 0 && endPoint.y == 15));
+
+    struct Point p = {0, 15};
+    solution.push(p);
+
+    // return solution;
     return solution;
 }
